602894346989afb7bece6b603bcf4cd9db7ccb9c
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2020-09-09 17:23:11 +0700

    Tambahan Form Harapan Kesehatan,Pendidikan,Komoditas & Edit Fertilitas

--- application/views/Surveyor/Survei.php
+++ application/views/Surveyor/Survei.php
@@ -46,7 +46,7 @@
               </div>
             </div>
             </div>
-            <div class="container-fluid border border-success rounded bg-light mb-1">
+            <div class="container-fluid border border-success rounded bg-light mb-1 mt-3">
             <div class="row">
               <div class="col-sm-12">
                 <div class="table-resp'ons'ive mt-1">
@@ -346,7 +346,7 @@
               </div>
             </div>
             </div>
-            <div class="container-fluid border border-primary rounded bg-light my-1">
+            <div class="container-fluid border border-primary rounded bg-light mb-1 mt-3">
             <div class="row">
               <div class="col-sm-6 my-1">
                 <div class="input-group">
@@ -432,7 +432,7 @@
               </div>
             </div>
             </div>
-            <div class="container-fluid border border-primary rounded bg-light my-1">
+            <div class="container-fluid border border-primary rounded bg-light mb-1 mt-3">
               <?php $Kesehatan = array('Dokter','Bidan Desa','Polides','Peralatan Persalinan','RS Swasta','Klinik Bersalin Swasta','Puskesmas',
                                        'Posyandu','Pemeriksaan Kehamilan','Vaksinasi','Pemberian Vitamin','Obat-Obatan','Biaya Persalinan'); 
               foreach ($Kesehatan as $key => $value) { ?>
@@ -467,8 +467,20 @@
                   </div>
                 </div>
               <?php }?>
+              <div class="row">
+                <div class="col-sm-auto my-1">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <label class="input-group-text bg-warning text-dark"><b>Jelaskan Beberapa Kendala Dalam Mendapatkan Fasilitas Kesehatan</b></label>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-12 my-1">
+                  <textarea id="KendalaKesehatan" rows="4" cols="119"></textarea>
+                </div>    
+              </div>
             </div>
-            <div class="container-fluid border border-primary rounded bg-light my-1">
+            <div class="container-fluid border border-primary rounded bg-light mb-1 mt-3">
               <?php $Pendidikan = array('Gedung Sekolah','Ruang Kelas','Guru','Buku Pelajaran','Jarak','Sekolah Swasta','Biaya Sekolah'); 
               foreach ($Pendidikan as $key => $value) { ?>
                 <div class="row">
@@ -502,8 +514,39 @@
                   </div>
                 </div>
               <?php }?>
+              <div class="row">
+                <div class="col-sm-auto my-1">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <label class="input-group-text bg-warning text-dark"><b>Tingkat Pendidikan Maksimal Yang Bisa Diusahakan</b></label>
+                    </div>
+                    <select class="custom-select" id="KesanggupanPendidikan">                    
+                      <option value="1">SD/Sederajat</option>
+                      <option value="2">SMP/Sederajat</option>
+                      <option value="3">SMA/Sederajat</option>
+                      <option value="4">Diploma</option>
+                      <option value="5">Sarjana</option>
+                      <option value="6">Pasca Sarjana</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-sm-auto my-1">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <label class="input-group-text bg-warning text-dark"><b>Keluhan Pendidikan</b></label>
+                    </div>
+                    <select class="custom-select" id="KeluhanPendidikan">                    
+                      <option value="1">Kurikulum Pendidikan</option>
+                      <option value="2">Biaya Sekolah</option>
+                      <option value="3">Ketersediaan Fasilitas Pendidikan</option>
+                      <option value="4">Jarak Fasilitas Pendidikan</option>
+                      <option value="5">Minat Bersekolah</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="container-fluid border border-primary rounded bg-light my-1">
+            <div class="container-fluid border border-primary rounded bg-light mb-1 mt-3">
               <?php $Komoditas = array('Beras','Tepung terigu','Ketela pohon/singkong','Kentang','Tongkol/tuna/cakalang','Kembung','Bandeng','Mujair','Mas','Lele','Ikan segar lainnya','Daging sapi','Daging ayam ras','Daging ayam kampung','Telur ayam ras','Susu kental manis','Susu bubuk','Susu bubuk bayi','Bayam','Kangkung','Kacang panjang','Bawang merah','Bawang putih','Cabe merah','Cabe rawit','Tahu','Tempe','Jeruk','Mangga','Salak','Pisang ambon','Pisang raja','Pisang lainnya','Pepaya','Minyak kelapa','Minyak goreng lainnya','Kelapa','Gula pasir','Teh','Kopi','Garam','Kecap','Penyedap masakan/vetsin','Mie instan','Roti manis/roti lainnya','Kue kering','Kue basah','Makanan gorengan','Gado-gado/ketoprak','Nasi campur/rames','Nasi goreng','Nasi putih','Lontong/ketupat sayur','Soto/gule/sop/rawon/cincang','Sate/tongseng','Mie bakso/mie rebus/mie goreng','Makanan ringan anak','Ikang (goreng/bakar dll)','Ayam/daging (goreng dll)','Makanan jadi lainnya','Air kemasan galon','Minuman jadi lainnya','Es lainnya','Roko kretek filter','Rokok kretek tanpa filter','Rokok putih','Perawatan rumah sendiri/bebas sewa','Rumah kontrak','Rumah sewa','Rumah dinas','Listrik','Air PAM','LPG','Minyak tanah','Lainnya(batu baterai,aki,korek,obat nyamuk dll)','Perlengkapan mandi','Barang kecantikan','Perawatan kulit,muka,kuku,rambut','Sabun cuci','Biaya RS Pemerintah','Biaya RS Swasta','Puskesmas/pustu','Praktek dokter/poliklinik','SPP','Bensin','Transportasi/pengangkutan umum','Pos dan Telekomunikasi','Pakaian jadi laki-laki dewasa','Pakaian jadi perempuan dewasa','Pakaian jadi anak-anak','Alas kaki','Minyak Pelumas','Meubelair','Peralatan Rumah Tangga','Perlengkapan perabot rumah tangga','Alat-alat Dapur/Makan'); 
                     $Satuan = array('Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','ml','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Kg','Liter','Liter','Butir','ons','ons','ons','ons','ml','ons','Bungkus','Bungkus','ons','buah','Bungkus','Bungkus','Bungkus','Bungkus','Bungkus','Bungkus','Piring','Piring','Piring','Bungkus','Kg','Kg','Bungkus','Gallon','ml','ml','Bungkus','Bungkus','Bungkus','Rp','Rp','Rp','Rp','Rp','M3','Kg','Liter','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Rp','Liter','Rp','Rp','Rp','Rp');
               foreach ($Komoditas as $key => $value) { ?>
@@ -525,6 +568,29 @@
                   </div>
                 </div>
               <?php }?>
+              <div class="row">
+                <div class="col-sm-6 my-1">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <label class="input-group-text bg-warning text-dark"><b>5 Komoditas Yang Memberatkan</b></label>
+                    </div>
+                    <input class="form-control" type="text" id="KomoditasMemberatkan">
+                  </div>
+                </div>
+                <div class="col-sm-auto my-1">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <label class="input-group-text bg-warning text-dark"><b>Penyebab Pengangguran</b></label>
+                    </div>
+                    <select class="custom-select" id="PenyebabPengangguran">                    
+                      <option value="1">Rendahnya Kualitas Pendidikan</option>
+                      <option value="2">Sedang Mencari Pekerjaan</option>
+                      <option value="3">Minimnya Lowongan Pekerjaan</option>
+                      <option value="4">Lainnya</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
@@ -606,32 +672,62 @@
 					}
         })
         
+        var ModeEditFertilitas = false
+        var IdEditAnak = 0
         var Fertilitas = []
         var OpsiFertilitas = ['Ya','Tidak']
 
         $("#SimpanFertilitas").click(function() {
-          var Anak = {}
-          Anak['AnakLahirHidup'] = $('#AnakLahirHidup').val()
-          Anak['AnakMasihHidup'] = $('#AnakMasihHidup').val()
-          Anak['AnakSudahMeninggal'] = $('#AnakSudahMeninggal').val()
-          Anak['UsiaAnak'] = $('#UsiaAnak').val()
-          Fertilitas.push(Anak)	
-          var	rows = ''
-          $.each(Fertilitas, function(key,value) {
-            rows = rows + '<tr>';
-            rows = rows + '<td class="text-center align-middle">'+(key+1)+'</td>';
-            rows = rows + '<td class="text-center align-middle">'+(key+1)+'</td>';
-            rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakLahirHidup-1]+'</td>';
-            rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakMasihHidup-1]+'</td>';
-            rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakSudahMeninggal-1]+'</td>';
-            rows = rows + '<td class="align-middle">'+value.UsiaAnak+'</td>';
-            rows = rows + '<td class="text-center align-middle">';
-              rows = rows + '<button EditFertilitas="'+key+'" class="btn btn-sm btn-warning mr-1 EditFertilitas"><i class="fas fa-edit"></i></button>';
-              rows = rows + '<button HapusFertilitas="'+key+'" class="btn btn-sm btn-danger ml-1 HapusFertilitas"><i class="fas fa-trash"></i></button>';
-            rows = rows + '</td>';
-            rows = rows + '</tr>';
-          })
-          $("#Fertilitas").html(rows)
+          if (ModeEditFertilitas) {
+            Fertilitas[IdEditAnak].AnakLahirHidup = $('#AnakLahirHidup').val()
+            Fertilitas[IdEditAnak].AnakMasihHidup = $('#AnakMasihHidup').val()
+            Fertilitas[IdEditAnak].AnakSudahMeninggal = $('#AnakSudahMeninggal').val()
+            Fertilitas[IdEditAnak].UsiaAnak = $('#UsiaAnak').val()
+            var	rows = ''
+            $.each(Fertilitas, function(key,value) {
+              rows = rows + '<tr>';
+              rows = rows + '<td class="text-center align-middle">'+(key+1)+'</td>';
+              rows = rows + '<td class="text-center align-middle">'+(key+1)+'</td>';
+              rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakLahirHidup-1]+'</td>';
+              rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakMasihHidup-1]+'</td>';
+              rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakSudahMeninggal-1]+'</td>';
+              rows = rows + '<td class="align-middle">'+value.UsiaAnak+'</td>';
+              rows = rows + '<td class="text-center align-middle">';
+                rows = rows + '<button EditFertilitas="'+key+'" class="btn btn-sm btn-warning mr-1 EditFertilitas"><i class="fas fa-edit"></i></button>';
+                rows = rows + '<button HapusFertilitas="'+key+'" class="btn btn-sm btn-danger ml-1 HapusFertilitas"><i class="fas fa-trash"></i></button>';
+              rows = rows + '</td>';
+              rows = rows + '</tr>';
+            })
+            $("#Fertilitas").html(rows) 
+            $('#AnakLahirHidup').val(1)
+            $('#AnakMasihHidup').val(1)
+            $('#AnakSudahMeninggal').val(1)
+            $('#UsiaAnak').val('')
+            ModeEditFertilitas = false
+          } else {
+            var Anak = {}
+            Anak['AnakLahirHidup'] = $('#AnakLahirHidup').val()
+            Anak['AnakMasihHidup'] = $('#AnakMasihHidup').val()
+            Anak['AnakSudahMeninggal'] = $('#AnakSudahMeninggal').val()
+            Anak['UsiaAnak'] = $('#UsiaAnak').val()
+            Fertilitas.push(Anak)	
+            var	rows = ''
+            $.each(Fertilitas, function(key,value) {
+              rows = rows + '<tr>';
+              rows = rows + '<td class="text-center align-middle">'+(key+1)+'</td>';
+              rows = rows + '<td class="text-center align-middle">'+(key+1)+'</td>';
+              rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakLahirHidup-1]+'</td>';
+              rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakMasihHidup-1]+'</td>';
+              rows = rows + '<td class="align-middle">'+OpsiFertilitas[value.AnakSudahMeninggal-1]+'</td>';
+              rows = rows + '<td class="align-middle">'+value.UsiaAnak+'</td>';
+              rows = rows + '<td class="text-center align-middle">';
+                rows = rows + '<button EditFertilitas="'+key+'" class="btn btn-sm btn-warning mr-1 EditFertilitas"><i class="fas fa-edit"></i></button>';
+                rows = rows + '<button HapusFertilitas="'+key+'" class="btn btn-sm btn-danger ml-1 HapusFertilitas"><i class="fas fa-trash"></i></button>';
+              rows = rows + '</td>';
+              rows = rows + '</tr>';
+            })
+            $("#Fertilitas").html(rows) 
+          }
         })
 
         $(document).on("click",".EditFertilitas",function(){
@@ -640,6 +736,8 @@
           $('#AnakMasihHidup').val(Edit.AnakMasihHidup)
           $('#AnakSudahMeninggal').val(Edit.AnakSudahMeninggal)
           $('#UsiaAnak').val(Edit.UsiaAnak)
+          IdEditAnak = $(this).attr('EditFertilitas')
+          ModeEditFertilitas = true
         })
 
         $(document).on("click",".HapusFertilitas",function(){
